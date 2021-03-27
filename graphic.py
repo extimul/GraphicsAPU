12efc6e7fbe775913973ecbfb8ceb1e7d80c1ecc
ExTimul <APlay819@gmail.com>
2021-03-27 23:06:50 +0300

    Added new graphics

--- graphic.py
+++ graphic.py
@@ -7,13 +7,17 @@ class APUGraphic:
     def __init__(self, time_line):
         self.time_line = time_line
         plt.style.use('seaborn-bright')
-        plt.tight_layout()
+        # plt.tight_layout()
 
-    def creat_graphic(self, graphic_name, args):
-        fig, ax = plt.subplots(figsize=(120, 10))
-        ax.plot(self.time_line, args[0], color=COLORS[0], marker='*', label=args[3])
-        ax.plot(self.time_line, args[1], color=COLORS[1], marker='o', label=args[4])
-        ax.plot(self.time_line, args[2], color=COLORS[2], marker='|', label=args[5])
+    def create_graphic(self, graphic_name, args):
+        fig, ax = plt.subplots(figsize=(10, 6))
+        if len(args) == 4:
+            ax.plot(self.time_line, args[0], color=COLORS[0], marker='*', label=args[2])
+            ax.plot(self.time_line, args[1], color=COLORS[1], marker='o', label=args[3])
+        else:
+            ax.plot(self.time_line, args[0], color=COLORS[0], marker='*', label=args[3])
+            ax.plot(self.time_line, args[1], color=COLORS[1], marker='o', label=args[4])
+            ax.plot(self.time_line, args[2], color=COLORS[2], marker='|', label=args[5])
         ax.set_xlabel('Time')
         ax.set_ylabel('Data')
         ax.legend(loc='best')
@@ -22,3 +26,5 @@ class APUGraphic:
         plt.show()
         fig.savefig(f'files/graphics/{graphic_name}.png', bbox_inches='tight')
         plt.close(fig)
+
+
--- main.py
+++ main.py
@@ -18,6 +18,9 @@ if __name__ == '__main__':
     gyrX_data, gyrY_data, gyrZ_data = parser.parse_gyr_values()
     accX_data, accY_data, accZ_data = parser.parse_acc_values()
     roll, pitch, yaw = parser.parse_orientation_values()
+    north, east = parser.parse_location_values()
+    height_data = parser.parse_height_values()
+    signal_data = parser.parse_signal_values()
 
     graphics = APUGraphic(time_line)
 
@@ -35,10 +38,12 @@ if __name__ == '__main__':
     })
 
     graphics_menu = MenuModel('Графики', GRAPHICS_MENU, {
-        1: lambda: graphics.creat_graphic("acc_graphic", [accX_data, accY_data, accZ_data, 'AccX', 'AccY', 'AccZ']),
-        2: lambda: graphics.creat_graphic("gyr_graphic", [gyrX_data, gyrY_data, gyrZ_data, 'GyrX', 'GyrY', 'GyrZ']),
-        3: lambda: graphics.creat_graphic("orientation_graphic", [roll, pitch, yaw, 'Roll', 'Pitch', 'Yaw']),
-        4: lambda: menu.go_back()
+        1: lambda: graphics.create_graphic("acc_graphic", [accX_data, accY_data, accZ_data, 'AccX', 'AccY', 'AccZ']),
+        2: lambda: graphics.create_graphic("gyr_graphic", [gyrX_data, gyrY_data, gyrZ_data, 'GyrX', 'GyrY', 'GyrZ']),
+        3: lambda: graphics.create_graphic("orientation_graphic", [roll, pitch, yaw, 'Roll', 'Pitch', 'Yaw']),
+        4: lambda: graphics.create_graphic("location_graphic", [north, east, 'North', 'East']),
+        5: lambda: graphics.create_graphic("sh_graphic", [height_data, signal_data, 'Height', 'Signal']),
+        6: lambda: menu.go_back()
     })
 
     menu.current_menu_obj = main_menu
